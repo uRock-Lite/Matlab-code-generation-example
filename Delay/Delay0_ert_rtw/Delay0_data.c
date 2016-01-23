9f4c082500ea8fcdb79e5d3dde357ee406e69d3f
victor22771659 <victor22771659@hotmail.com>
2016-01-24 04:38:38 +0800

    delay effect example

---
+++ Delay/Delay0_ert_rtw/Delay0_data.c
@@ -0,0 +1,33 @@
+/*
+ * File: Delay0_data.c
+ *
+ * Code generated for Simulink model 'Delay0'.
+ *
+ * Model version                  : 1.7
+ * Simulink Coder version         : 8.7 (R2014b) 08-Sep-2014
+ * C/C++ source code generated on : Sun Jan 24 03:25:52 2016
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: 32-bit Generic
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "Delay0.h"
+#include "Delay0_private.h"
+
+/* Block parameters (auto storage) */
+P_Delay0_T Delay0_P = {
+  441.0,                               /* Expression: 441
+                                        * Referenced by: '<S1>/Gain1'
+                                        */
+  0.5                                  /* Expression: 0.5
+                                        * Referenced by: '<S1>/Gain'
+                                        */
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
